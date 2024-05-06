19b979d05506339fef97cfa02c077e566a943b68
Jordan Huynh <jordanhuynh784@gmail.com>
2024-05-06 00:59:30 -0700

    Update

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 https://frndlydragon.github.io/Lab5_Starter/expose.html
 <br>
 https://frndlydragon.github.io/Lab5_Starter/explore.html
+
+## Lab Questions
+1. No since this would be complex with many parts working together (clientside with both users creating their messages, network to send those messages, and client again to receive messages).
+2. Yes since this would only test one aspect of a part of the feature/application that doesn't need to communicate with other parts of the application. One could simply make a case to judge the character limit easily.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,82 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+test('invalid phone number 1', () => {
+  expect(isPhoneNumber('hello')).toBe(false);
+});
+
+test('invalid phone number 2', () => {
+  expect(isPhoneNumber()).toBe(false);
+});
+
+test('valid phone number 1', () => {
+  expect(isPhoneNumber('(111) 111-1111')).toBe(true);
+});
+
+test('valid phone number', () => {
+  expect(isPhoneNumber('123-456-7890')).toBe(true);
+});
+
+test('invalid email 1', () => {
+  expect(isEmail('not_an_email')).toBe(false);
+});
+
+test('invalid email 2', () => {
+  expect(isEmail('definitelynot@email')).toBe(false);
+});
+
+test('valid email 1', () => {
+  expect(isEmail('email@domain.com')).toBe(true);
+});
+
+test('valid email 2', () => {
+  expect(isEmail('interesting@bigol.gov')).toBe(true);
+});
+
+test('invalid password 1', () => {
+  expect(isStrongPassword('1badpassword')).toBe(false);
+});
+
+test('invalid password 2', () => {
+  expect(isStrongPassword('no good!')).toBe(false);
+});
+
+test('valid password 1', () => {
+  expect(isStrongPassword('password1')).toBe(true);
+});
+
+test('valid password 2', () => {
+  expect(isStrongPassword('pa55word1')).toBe(true);
+});
+
+test('invalid date 1', () => {
+  expect(isDate('10-10-1001')).toBe(false);
+});
+
+test('invalid date 2', () => {
+  expect(isDate('10/10/21')).toBe(false);
+});
+
+test('valid date 1', () => {
+  expect(isDate('02/11/2024')).toBe(true);
+});
+
+test('valid date 2', () => {
+  expect(isDate('2/4/2025')).toBe(true);
+});
+
+test('invalid color 1', () => {
+  expect(isHexColor('#5555')).toBe(false);
+});
+
+test('invalid color 2', () => {
+  expect(isHexColor('#5555555')).toBe(false);
+});
+
+test('valid color 1', () => {
+  expect(isHexColor('#f0f0f0')).toBe(true);
+});
+
+test('valid color 2', () => {
+  expect(isHexColor('#B5B')).toBe(true);
+});
